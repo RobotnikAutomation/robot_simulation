03fc4b362103d780fa8277aeb4a23cc5ae403eaf
Carlos Gascon <cgascon@robotnik.es>
2025-04-03 12:57:20 +0200

    Feat/ros2/update control jazzy (#13)
    * Change control files for jazzy
    
    * Add arm condition to start joint trajectory controller
    
    * add imu sensor plugin to world.sdf
    
    * update package.xml with dependencies
    
    * update readme to gazebo ignition and jazzy
    
    * add ur_description to readme
    
    * update readme

--- robotnik_gazebo_ignition/launch/spawn_robot.launch.py
+++ robotnik_gazebo_ignition/launch/spawn_robot.launch.py
@@ -31,6 +31,7 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.descriptions import ParameterValue
 from robotnik_common.launch import ExtendedArgument, AddArgumentParser
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -113,6 +114,13 @@ def generate_launch_description():
         default_value='0.0',
     )
     add_to_launcher.add_arg(arg)
+    
+    arg = ExtendedArgument(
+        name='has_arm',
+        description='If robot has an arm to start controller',
+        default_value='False',
+    )
+    add_to_launcher.add_arg(arg)
     params = add_to_launcher.process_arg()
 
     robot_dir = os.path.join(get_package_share_directory('robotnik_description'), 'launch')
@@ -160,26 +168,6 @@ def generate_launch_description():
     )
     ld.add_action(ros_gz_bridge)
 
-    # ros_gz_image_bridge = Node(
-    #     package="ros_gz_image",
-    #     executable="image_bridge",
-    #     arguments=[
-    #         "/robot/front_rgbd_camera/color/image_raw", 
-    #         "/robot/rear_rgbd_camera/color/image_raw"
-    #         #"/robot/front_rgbd_camera/ired1/image_raw", 
-    #         #"/robot/rear_rgbd_camera/ired1/image_raw",
-    #         #"/robot/front_rgbd_camera/ired2/image_raw", 
-    #         #"/robot/rear_rgbd_camera/ired2/image_raw",
-    #         #"/robot/front_rgbd_camera/depth/image_raw",
-    #         #"/robot/rear_rgbd_camera/depth/image_raw"
-    #     ],
-    #     namespace=params['namespace']
-    # )
-    # ld.add_action(ros_gz_image_bridge)
-
-    # controller_dir = os.path.join(get_package_share_directory('robotnik_controller'), 'launch')
-
-    
     joint_state_broadcaster = Node(
         package='controller_manager',
         executable='spawner',
@@ -187,6 +175,27 @@ def generate_launch_description():
         namespace=params['namespace']
     )
     ld.add_action(joint_state_broadcaster)
+    
+    joint_trajectory_controller= Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_trajectory_controller'],
+        output='screen',
+        emulate_tty=True,
+        namespace=params['namespace'],
+        condition=IfCondition(params['has_arm'])
+    )
+
+    init_joint_trajectory_controller = RegisterEventHandler(
+        OnProcessExit(
+            target_action=joint_state_broadcaster,
+            on_exit=[
+                LogInfo(msg='Joint States spawned'),
+                joint_trajectory_controller
+            ]
+        )
+    )
+    ld.add_action(init_joint_trajectory_controller)
 
     robotnik_controller= Node(
         package='controller_manager',
