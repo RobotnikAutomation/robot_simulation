f762c61a81711ee140290f0b68d4563150ca74fc
cgasconb <cgascon@robotnik.es>
2025-04-04 13:59:52 +0200

    add bridge for all robots

--- robotnik_gazebo_ignition/launch/spawn_robot.launch.py
+++ robotnik_gazebo_ignition/launch/spawn_robot.launch.py
@@ -154,15 +154,13 @@ def generate_launch_description():
             namespace=params['namespace']
     )
     ld.add_action(robot_spawner)
-    bridge_params = os.path.join(get_package_share_directory('robotnik_gazebo_ignition'),'config','bridge.yaml')
 
+    bridge_params = [get_package_share_directory('robotnik_gazebo_ignition'),'/config/', robot,'/bridge.yaml']
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
-        arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}'
+        parameters=[
+            {'config_file': bridge_params},
         ],
         namespace=params['namespace']
     )
