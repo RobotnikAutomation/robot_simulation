e0b1c22045dff2a5b588b10d528ddb4fd2db361a
Rafael Martin <rmartin@robotnik.es>
2025-10-14 11:09:08 +0200

    add headless

--- robotnik_o3de/launch/spawn_world.launch.py
+++ robotnik_o3de/launch/spawn_world.launch.py
@@ -1,52 +1,52 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, OpaqueFunction, RegisterEventHandler, EmitEvent
+from launch.actions import ExecuteProcess, OpaqueFunction, RegisterEventHandler, EmitEvent, DeclareLaunchArgument
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
+from launch.substitutions import LaunchConfiguration
 
-from robotnik_common.launch import AddArgumentParser, ExtendedArgument
 
-def _start_game(context, executable, world):
+def _start_game(context, executable, world, gui):
     world = world.perform(context)
     world_path = f"Levels/{world}/{world}.spawnable"
+    gui = gui.perform(context)
+
+    extended_args = []
+    if gui.lower() != "true":
+        extended_args.append("-console-mode")
 
     cfg_path = "/tmp/game.cfg"
     with open(cfg_path, "w") as f:
+        print(f"Writing content {world_path} to {cfg_path}")
         f.write(f"LoadLevel {world_path}\n")
 
     proc = ExecuteProcess(
-        cmd=[executable, f"--console-command-file={cfg_path}"],
+        cmd=[executable, f"--console-command-file={cfg_path}", *extended_args],
         name="robotnik_game_launcher",
-        output="screen",
+        output="log",
         shell=False,
+        emulate_tty=True,
     )
 
-    # mimic on_exit_shutdown=true
     shutdown_on_exit = RegisterEventHandler(
         OnProcessExit(target_action=proc, on_exit=[EmitEvent(event=Shutdown())])
     )
-
     return [proc, shutdown_on_exit]
-
 
 def generate_launch_description():
     ld = LaunchDescription()
-    add = AddArgumentParser(ld)
+    ld.add_action(DeclareLaunchArgument("world", default_value="demo", description="world name"))
+    ld.add_action(DeclareLaunchArgument("gui", default_value="true", description="enable gui"))
 
     executable = "/home/robotnik/projects/robotnik_roscon25/build/linux/bin/profile/robotnik_roscon25.GameLauncher"
-
-    add.add_arg(ExtendedArgument(name="world", description="world name", default_value="demo"))
-    add.add_arg(ExtendedArgument(name="gui", description="enable gui", default_value="true"))
-
-    params = add.process_arg()  # dict of LaunchConfiguration
 
     ld.add_action(
         OpaqueFunction(
             function=_start_game,
             kwargs={
                 "executable": executable,
-                "world": params["world"],
+                "world": LaunchConfiguration("world"),
+                "gui": LaunchConfiguration("gui"),
             },
         )
     )
-
     return ld
