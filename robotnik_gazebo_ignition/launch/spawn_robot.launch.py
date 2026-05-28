b7af3c1ef4e3a123a89259d02b11856b6e2073c2
Jose Luis Galán Avilés <jlgalan@robotnik.es>
2026-05-28 11:27:03 +0200

    Refactor jazzy-devel installation docs, dependency manifests and development Docker workflow (#70)
    * Fix dependencies for jazzy devel
    
    * First review and refactor for better explanation
    
    * Fix dependencies for package.xml, add devel.repos for developers and estructure dependencies in REadme.md
    
    * Fix readme commands and compatibility
    
    * Fix rbsummit_steel
    
    * Final readme review
    
    * Rename .jazzy repos for better understanding
    
    * Add docker build for devel installation
    
    * Add docker functional version for devel
    
    * Add teleop panel
    
    * Add teleop repos
    
    * Final fixes docker and prepared for merge
    
    * Standar naming for folder robotnik_teleop_panel
    
    * Fix dependencies for spawn
    
    * Fix dependencies for moveit
    
    * Fix dependencies for teleop panel
    
    * Split troubleshooting and nvidia config in a different .md
    
    * Remove dependencies for use correctly the rosdep install
    
    * Add larger explanation for normal problems
    
    * Fix navigation dependencies
    
    * Update repos of moveit_configs and description for match correct dependencies

--- robotnik_gazebo_ignition/launch/spawn_robot.launch.py
+++ robotnik_gazebo_ignition/launch/spawn_robot.launch.py
@@ -251,7 +251,7 @@ def launch_setup(context, params):
     new_controllers = extract_controllers_from_yaml(path)
 
     # ROS2 control
-    controllers = ['joint_state_broadcaster']
+    controllers =  ['--controller-manager-timeout', '60', '--service-call-timeout', '60', 'joint_state_broadcaster']
     # Replace default joint_state_broadcaster by the one defined in the specific
     # ros2_control.yaml for the robot model
     if 'joint_state_broadcaster' in new_controllers:
