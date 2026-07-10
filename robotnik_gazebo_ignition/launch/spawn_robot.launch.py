63be9ffc93e509cb0a529e6429715316ddb4e5cf
Ángel Soriano <asoriano@robotnik.es>
2026-07-10 07:57:57 +0200

    custom ConfigFile script replaced by allow_substs from launch_ros

--- robotnik_gazebo_ignition/launch/spawn_robot.launch.py
+++ robotnik_gazebo_ignition/launch/spawn_robot.launch.py
@@ -39,6 +39,7 @@ from launch.substitutions import PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterFile
 
 from robotnik_common.launch import AddArgumentParser, ExtendedArgument
 
@@ -46,72 +47,13 @@ from robotnik_common.launch import AddArgumentParser, ExtendedArgument
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import Union, Optional
-from launch import SomeSubstitutionsType, SomeSubstitutionsType_types_tuple
+from launch import SomeSubstitutionsType, SomeSubstitutionsType_types_tuple, LaunchContext
 from launch.frontend.parse_substitution import parse_substitution
 from launch.utilities import normalize_to_list_of_substitutions, perform_substitutions
 from launch.utilities.typing_file_path import FilePath
 from launch.substitution import Substitution
-
-from launch import LaunchContext
 from launch.conditions import IfCondition, UnlessCondition
 
-
-# TODO: move this utility class into robotnik_common
-class ConfigFile(Substitution):
-    """Substitution to get the path of the configuration file."""
-
-    def __init__(
-        self,
-        param_file: Union[FilePath, SomeSubstitutionsType],
-    ) -> None:
-        """
-        Construct a parameter file description.
-
-        :param param_file: The path to the parameter file or a substitution that resolves to it.
-        """
-        self.__evaluated_param_file: Optional[Path] = None
-        self.__created_tmp_file = False
-
-        self.__param_file = param_file
-        if isinstance(param_file, SomeSubstitutionsType_types_tuple):
-            self.__param_file = normalize_to_list_of_substitutions(param_file)  # type: ignore
-
-    def perform(self, context: LaunchContext) -> str:
-        """Substitute the parameter file path."""
-        param_file = self.__param_file
-        if isinstance(param_file, list):
-            # list of substitutions
-            param_file = perform_substitutions(context, self.__param_file)  # type: ignore
-
-        param_file_path: Path = Path(param_file)  # type: ignore
-        with open(param_file_path, 'r') as f, NamedTemporaryFile(
-                mode='w', prefix='launch_params_', delete=False
-            ) as h:
-                parsed = perform_substitutions(context, parse_substitution(f.read()))  # type: ignore
-                try:
-                    yaml.safe_load(parsed)
-                except Exception:
-                    raise SubstitutionFailure(
-                        'The substituted parameter file is not a valid yaml file')
-                h.write(parsed)
-                param_file_path = Path(h.name)
-                self.__created_tmp_file = True
-        self.__evaluated_param_file = param_file_path
-        return str(param_file_path)
-
-    def cleanup(self) -> None:
-        """Remove the temporary file if it was created."""
-        if self.__created_tmp_file and self.__evaluated_param_file is not None:
-            try:
-                self.__evaluated_param_file.unlink()
-            except FileNotFoundError:
-                # The file may have been deleted already, ignore this error
-                pass
-            self.__evaluated_param_file = None
-
-    def __del__(self):
-        """Clean up the temporary file when the object is deleted."""
-        self.cleanup()
 
 
 def load_yaml(package_path, relative_path):
@@ -263,12 +205,7 @@ def launch_setup(context, params):
     controllers.extend(new_controllers)
     print("Controllers to be spawned:", controllers)
 
-    robot_controller_config = ConfigFile(path)
-
-    controllers.append('--param-file')
-    controllers.append(
-         robot_controller_config, # type: ignore
-    )
+    robot_controller_config = ParameterFile(path, allow_substs=True)
 
     is_rbcar = EqualsSubstitution(params['robot'], 'rbcar')
 
@@ -277,6 +214,7 @@ def launch_setup(context, params):
         executable='spawner',
         namespace=params['robot_id'],
         arguments=controllers,
+        parameters=[robot_controller_config],
         output='screen',
         condition=UnlessCondition(is_rbcar),
     ))
@@ -285,13 +223,13 @@ def launch_setup(context, params):
         '--controller-manager-timeout', '60',
         '--service-call-timeout', '60',
         'joint_state_broadcaster',
-        '--param-file', robot_controller_config,  # type: ignore
     ]
     ret.append(Node(
         package='controller_manager',
         executable='spawner',
         namespace=params['robot_id'],
         arguments=rbcar_joint_state_broadcaster,
+        parameters=[robot_controller_config],
         output='screen',
         condition=IfCondition(is_rbcar),
     ))
@@ -300,7 +238,6 @@ def launch_setup(context, params):
         '--controller-manager-timeout', '60',
         '--service-call-timeout', '60',
         'robotnik_base_control',
-        '--param-file', robot_controller_config,  # type: ignore
         '--controller-ros-args', '--ros-args -r ~/tf_odometry:=/tf -r ~/odometry:=~/odom -r ~/reference:=~/cmd_vel',
     ]
     ret.append(Node(
@@ -308,6 +245,7 @@ def launch_setup(context, params):
         executable='spawner',
         namespace=params['robot_id'],
         arguments=rbcar_ackermann_controller,
+        parameters=[robot_controller_config],
         output='screen',
         condition=IfCondition(is_rbcar),
     ))
